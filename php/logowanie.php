44e987596ce249e005fec3c7dd75153c5dbf7b2e
Mateusz-debug-tech <63314520+Mateusz-debug-tech@users.noreply.github.com>
2020-05-12 17:16:53 +0200

    Add files via upload

---
+++ php/logowanie.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Logowanie</title>
+  <link rel="stylesheet" href="logow.css">
+</head>
+<body>
+
+
+  <div id="logowanie">
+	
+	<div id="logo">Veloxis</div>
+	<div id="pod">&nbsp;logowanie</div>
+	
+    <form>
+       <input type="text" placeholder="Nazwa użytkownika:"/>
+       <input type="password" placeholder="Hasło:"/>
+	   <div class="s1">
+	   <a href="url">Zapomniałeś hasła?</a>
+	   </div>
+       <input type="submit" value="Zarejstruj"/>
+    </form>
+  </div>
+
+</body>
+
+</html>
+
