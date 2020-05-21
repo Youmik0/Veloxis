57c2072307f99df380f36e4ab06fb9d042e60122
Mateusz-debug-tech <63314520+Mateusz-debug-tech@users.noreply.github.com>
2020-05-21 17:23:53 +0200

    Update logowanie.php

--- php/logowanie.php
+++ php/logowanie.php
@@ -9,20 +9,29 @@
 <body>
   <div id="logowanie">
 	
-	<div id="logo"><a href="../php/veloxis.php">Veloxis</a></div>
+	<div id="logo">Veloxis</div>
 	<div id="pod">&nbsp;logowanie</div>
 	
     <form method="post" action="logowanie.php">
-	<?php include('errors.php'); ?>
+	
        <input type="text" name="nazwa_uzytkownika" placeholder="Nazwa użytkownika:"/>
+	   <?php if(!empty($_user_error)) echo $_user_error; ?>
        <input type="password" name="haslo" placeholder="Hasło:"/>
+	   <?php if(!empty($_password_error)) echo $_password_error; ?>
 	   <div class="s1">
 	   <a href="url">Zapomniałeś hasła?</a>
 	   </div>
-       <button type="submit" class="btn" name="login_user">Logowanie</button>
+       <button type="submit" class="btn" name="login_user">Zaloguj</button>
     </form>
 	<br>
 	<a href="../php/rejestracja.php">Nie masz jeszcze konta? Przejdź do Rejestracji.<a>
   </div>
+  <script>
+	document.getElementById("logo").addEventListener("click", toM);
+
+	function toM() {
+	window.location.href="../php/veloxis.php";
+	}
+	</script>
 </body>
 </html>
