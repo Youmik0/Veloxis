18540f4e867e63f4b19a5ae939db36a04121a48d
Youmik0 <nimokmaster@gmail.com>
2020-05-27 16:23:06 +0200

    Update zrob.php

--- php/zrob.php
+++ php/zrob.php
@@ -4,7 +4,7 @@ $db->set_charset("utf8");
 
 $Values= unserialize(urldecode($_GET['vals']));
 echo "<h2>Your Input:</h2>";
-echo "nazwa:" .$Values['nazwa']; //Here what needs to be written after . and before ; to use the values passed from the other file
+echo "nazwa:" .$Values['nazwa'];
 echo "<br>";
 echo "opis:" .$Values['opis'];
 echo '<br>';
@@ -16,17 +16,19 @@ echo "id_uzytkownika:" .$Values['id_uzytkownika'];
 echo '<br>';
 echo "id_zdjecia:" .$Values['id_zdjecia'];
 echo '<br>';
-$nazwa =$Values['nazwa'];
-$opis =$Values['opis'];
+$nazwa=$Values['nazwa'];
+$opis=$Values['opis'];
 $cena=$Values['cena'];
 $kategoria=$Values['kategoria'];
 $id_uzytkownika=$Values['id_uzytkownika'];
 $id_zdjecia=$Values['id_zdjecia'];
 $nazwa_zdjecia=$Values['nazwa_zdjecia'];
-
-$updateoferty="UPDATE oferty SET id_zdjecia='$id_zdjecia', nazwa_zdjecia='$nazwa_zdjecia' WHERE nazwa_oferty='$nazwa' AND opis='$opis' AND cena='$cena' AND id_kategorii='$kategoria' AND id_uzytkownika='$id_uzytkownika'";
-if(mysqli_query($db,$updateoferty))
-{
-	header('Location: veloxis.php');
+$premium=$Values['premium'];
+mysqli_query($db,"SET FOREIGN_KEY_CHECKS=0;");
+$updateoferty="UPDATE oferty SET id_zdjecia='$id_zdjecia', nazwa_zdjecia='$nazwa_zdjecia' WHERE nazwa_oferty='$nazwa' AND opis='$opis' AND cena='$cena' AND id_kategorii='$kategoria' AND id_uzytkownika='$id_uzytkownika' AND premium='$premium'";
+if(mysqli_query($db,$updateoferty)){
+mysqli_query($db,"SET FOREIGN_KEY_CHECKS=1;");
+header('Location: veloxis.php');
 }
+mysqli_query($db,"SET FOREIGN_KEY_CHECKS=1;");
 ?>
