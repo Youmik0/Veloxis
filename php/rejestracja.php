3f4be30bedc55d4bb30c1bdd9db93f66eadda6fc
Mateusz-debug-tech <63314520+Mateusz-debug-tech@users.noreply.github.com>
2020-05-30 19:23:02 +0200

    Update rejestracja.php

--- php/rejestracja.php
+++ php/rejestracja.php
@@ -19,13 +19,23 @@
 	   <?php if(!empty($_name_error)) echo $_name_error; ?>
        <input type="text" name="nazwisko" placeholder="Nazwisko: ">
 	   <?php if(!empty($_lastn_error)) echo $_lastn_error; ?>
-	   <input type="tel" name="telefon" placeholder="Telefon: " pattern="[0-9]{9}" required>
+	   <input type="text" name="miejscowosc" placeholder="Miejscowosc: ">
+	   <?php if(!empty($_m_error)) echo $_m_error; ?>
+	   <input type="text" name="ulica" placeholder="Ulica: ">
+	   <?php if(!empty($_u_error)) echo $_u_error; ?>
+	   <input type="text" name="nr_domu" placeholder="Nr. domu/mieszkania: ">
+	   <?php if(!empty($_n_error)) echo $_n_error; ?>
+	   <input type="text" name="kod_po" pattern="[0-9]{2}\-[0-9]{3}" title="Kod pocztowy wymagany w następującym formacie NP: 12-345" placeholder="Kod pocztowy: ">
+	   <?php if(!empty($_k_error)) echo $_k_error; ?>
+	   <input type="text" name="numer_bank" pattern="[0-9]{26}" title="Potrzebne jest 26 cyfr" placeholder="Numer konta bankowego: ">
+	   <?php if(!empty($_b_error)) echo $_b_error; ?>
+	   <input type="tel" name="telefon" placeholder="Telefon: " pattern="[0-9]{9}" title="Potrzebne jest 9 cyfr" required>
 	   <?php if(!empty($_phone_error)) echo $_phone_error; ?>
        <input type="email" name="email" placeholder="Email: ">
 	   <?php if(!empty($_mail_error)) echo $_mail_error; if(!empty($_mail2_error)) echo $_mail2_error;?>
-       <input type="password" name="haslo1" placeholder="Hasło: ">
+       <input type="password" name="haslo1" pattern=".{8,}" title="8 lub więcej znaków" placeholder="Hasło: ">
 	   <?php if(!empty($_pas_error)) echo $_pas_error; ?>
-	   <input type="password" name="haslo2" placeholder="Powtórz hasło: ">
+	   <input type="password" name="haslo2" pattern=".{8,}" title="8 lub więcej znaków" placeholder="Powtórz hasło: ">
 	   <?php if(!empty($_pass2_error)) echo $_pass2_error; ?>
 	   <div class="s1">
 	   <input type="checkbox" name="reg" id="reg">
