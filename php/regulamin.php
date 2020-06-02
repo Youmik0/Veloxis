8798e646833b8b5094db49269a09654a53b495b2
Youmik0 <nimokmaster@gmail.com>
2020-06-02 23:35:32 +0200

    Add files via upload

---
+++ php/regulamin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../css/re.css">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<div id="topbar"><div id="logo">Veloxis</div></div>
+<div id="cont">
+<div id="rejestracja">
+<strong>Warunki korzystania z Veloxis </strong><br>
+<strong>1.</strong> Użytkownikami mogą być osoby fizyczne, które ukończyły 18 lat i posiadają pełną zdolność do czynności prawnych. <br>
+<strong>2.</strong> W ramach Veloxis, Sprzedający określając warunki Oferty może udostępnić Kupującym następujące tryby zawarcia umowy sprzedaży: <br>
+<strong>a.</strong> Wyłącznie z opcją Kup Teraz, w ramach której Sprzedający oferuje sprzedaż Towaru po określonej z góry cenie. <br>
+<strong>b.</strong> Licytacja, w ramach której Sprzedający poprzez wystawienie Towaru zaprasza Licytantów do składania ofert celem zawarcia umowy. <br>
+<strong>3.</strong> Sprzedający zobowiązują się do niewystawiania na Transakcji Towarów, którymi obrót narusza obowiązujące przepisy prawa lub uprawnienia osób trzecich (w szczególności prawa autorskie i inne prawa własności intelektualnej), jak również których wystawienie może być uznane za naruszające dobre obyczaje. <br>
+<strong>4.</strong> Z chwilą wzięcia udziału w Licytacji, dane osobowe w zakresie nazwy Konta (loginu) Licytanta są przekazywane Sprzedającemu. <br>
+<strong>5.</strong> Oferty złożone przez Licytantów wiążą uczestników Licytacji, chyba że Sprzedający dokona ich odrzucenia. Odrzucenie oferty Licytanta może nastąpić wyłącznie przed zakończeniem Licytacji. <br>
+<strong>6.</strong> Propozycja zawarcia umowy ze Sprzedającym, złożona przez Licytanta, którego Konto zostanie zawieszone przed zamknięciem Licytacji, przestaje wiązać. Informacja o zawieszeniu Konta Licytanta jest uwidaczniana na stronie Licytacji. <br>
+<strong>7.</strong> Veloxis nie jest stroną umów sprzedaży Towarów zawieranych pomiędzy Użytkownikami i nie gwarantuje, że Sprzedający oraz Kupujący są uprawnieni do zawarcia i wykonania umowy. <br>
+<strong>8.</strong> Veloxis.pl nie ponosi odpowiedzialności za zachowania Użytkowników w ramach Veloxis ani za nienależyte wykonanie bądź niewykonanie przez nich umów zawartych w ramach Transakcji, jak również za następstwa działań podjętych przez Użytkowników oraz osoby trzecie, a stanowiących naruszenie postanowień Regulaminu. Veloxis.pl w szczególności nie ponosi odpowiedzialności za jakość, bezpieczeństwo lub legalność Towarów sprzedawanych w ramach Oferty, zdolność Sprzedających do sprzedaży, wypłacalność Kupujących oraz prawdziwość i rzetelność informacji, oraz innych treści podawanych/przekazywanych przez Użytkowników w ramach Veloxis.<br> 
+<strong>9.</strong> W przypadku, gdy działania Użytkownika naruszają postanowienia Regulaminu, Veloxis.pl może: <br>
+<strong>a.</strong> upomnieć Użytkownika za pośrednictwem poczty elektronicznej; <br>
+<strong>b.</strong> udzielić Użytkownikowi ostrzeżenia za pośrednictwem poczty elektronicznej; <br>
+<strong>c.</strong> zablokować konto Użytkownikowi na czas określony lub na czas nieokreślony. <br>
+<strong>10.</strong> Usługi świadczone przez Veloxis.pl w ramach Veloxis są odpłatne. Wszystkimi opłatami i prowizjami obciążany jest Sprzedający. Należności te naliczane są na bieżąco i przedstawiane do zapłaty za kolejne okresy rozliczeniowe. Okresem rozliczeniowym jest miesiąc kalendarzowy. <br>
+<br>Dane kontaktowe:<br>
+<strong>Email:</strong> veloxis@gmail.com<br>
+<strong>Telefon:</strong> 420690123<br>
+
+</div>
+</div>
+<script>
+	document.getElementById("logo").addEventListener("click", toM);
+
+	function toM() {
+	window.location.href="../php/veloxis.php";
+	}
+	</script>
+</body>
+
+</html>
